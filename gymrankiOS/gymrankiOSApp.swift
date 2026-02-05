0a83aed21de401a12bc0b2fa679beabfe66c1c1e
Backhaus <tinchoignacio33@gmail.com>
2026-02-05 10:28:24 -0300

    Initial Commit

---
+++ gymrankiOS/gymrankiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  gymrankiOSApp.swift
+//  gymrankiOS
+//
+//  Created by Martin Backhaus on 05/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct gymrankiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gymrankiOSTests/gymrankiOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  gymrankiOSTests.swift
+//  gymrankiOSTests
+//
+//  Created by Martin Backhaus on 05/02/2026.
+//
+
+import Testing
+@testable import gymrankiOS
+
+struct gymrankiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
